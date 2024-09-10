cdb6564b58e118e0c8797fec22c549cc99acdf2b
Lydia Pankratova <ecodeltaplan@gmail.com>
2024-09-10 17:08:04 +0300

    Execute migrations

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,12 +3,14 @@ using GameStore.Api.Endpoints;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddDbContext<GameStoreContext>(options => 
         options.UseSqlite(connString));
 
+var app = builder.Build();
+
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
